3d2d6c7ae72f31e0fee71f6d0d86f0495acb7ae0
Kevin Nguyen <knguyen@appdev.yearup.org>
2025-04-23 15:50:15 -0500

    modified payroll calc to inclue bufferwriter

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
@@ -2,19 +2,38 @@ package com.pluralsight;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class PayrollCalc {
+    static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
         try{
-
-            FileReader employeeFile = new FileReader("src/main/resources/employees.csv");
-
+            //file name
+            String employeeInfo = "employees.csv";
+            String outputFile = "payroll-sept-2023.csv";
+            //file reader
+            FileReader employeeFile = new FileReader("src/main/resources/" + employeeInfo);
             BufferedReader employeeList = new BufferedReader(employeeFile);
-            String employeeInfo;
-            while((employeeInfo = employeeList.readLine()) != null){
-                String[] employeeDetails = employeeInfo.split("\\|");
+            //file writer
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + outputFile);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.write("ID:, Employee:, Gross Pay:\n");
+
+            System.out.println("What is the file you would like to process?");
+            employeeInfo = myScanner.nextLine();
+
+            System.out.println("What is the file you would like to create?");
+            outputFile = myScanner.nextLine();
+
+
+            String theLine;
+
+            while((theLine = employeeList.readLine()) != null){
+                String[] employeeDetails = theLine.split("\\|");
                 if (employeeDetails[0].equalsIgnoreCase("id")){
                     continue;
                 }
@@ -26,9 +45,14 @@ public class PayrollCalc {
                 Employee theEmployee = new Employee(id, names, hoursWorked, payRate);
                 System.out.printf("ID: %d | Name: %s | Gross: $%.2f%n", theEmployee.getEmployeeId(), theEmployee.getEmployeeName(),theEmployee.getGrossPay());
 
+                String outputFileLine = theEmployee.getEmployeeId() + ", " + theEmployee.getEmployeeName() + ", " + theEmployee.getGrossPay() + "\n";
+                bufWriter.write(outputFileLine);
+
             }
 
+            bufWriter.close();
             employeeList.close();
+
 
         } catch (Exception e) {
             e.printStackTrace();
